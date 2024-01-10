29f7f5922cd87a8d0075e28a809ab62db410c269
LAPTOP-BLT1CON4\小徐同学 <3233885648@qq.com>
2024-01-10 15:02:52 +0800

    xjh，feat:init

--- chatbot-api-interface/src/test/java/xjh/chatbot/api/test/ApiTest.java
+++ chatbot-api-interface/src/test/java/xjh/chatbot/api/test/ApiTest.java
@@ -1,4 +1,14 @@
 package xjh.chatbot.api.test;
 
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.junit.Test;
+
 public class ApiTest {
+    @Test
+    public void query_unanswered(){
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        HttpGet get = new HttpGet();
+    }
 }
